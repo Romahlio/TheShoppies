da2230c7b82e014696019c19f6a9f703014865ac
Romahlio <r26willi@uwaterloo.ca>
2020-09-09 00:07:11 -0400

    Updates

---
+++ precache-manifest.bc9f142cc1c19350aa2d62e18a1f207f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596974b8e2b8260622b17e3ed5707653",
+    "url": "/TheShoppies/index.html"
+  },
+  {
+    "revision": "fd886181476d6bd38dce",
+    "url": "/TheShoppies/static/css/main.eac59f64.chunk.css"
+  },
+  {
+    "revision": "640801ac7467dec7c9aa",
+    "url": "/TheShoppies/static/js/2.3eb0f3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheShoppies/static/js/2.3eb0f3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd886181476d6bd38dce",
+    "url": "/TheShoppies/static/js/main.2e3c0474.chunk.js"
+  },
+  {
+    "revision": "3c2b2620361e165d795f",
+    "url": "/TheShoppies/static/js/runtime-main.bb1825b9.js"
+  },
+  {
+    "revision": "8e8fd3fa6c0fb1110b0bd49db6218551",
+    "url": "/TheShoppies/static/media/IMFellFrenchCanonSC-Regular.8e8fd3fa.ttf"
+  },
+  {
+    "revision": "abd01a79bd4c8d6617422b796937253a",
+    "url": "/TheShoppies/static/media/Sitka.abd01a79.ttc"
+  }
+]);
